87c37048664e52856e735b17bfe4ca30d75a7c2b
Nazzareno Pasqual <79537141+TerribleCoding@users.noreply.github.com>
2021-04-21 19:49:52 +0200

    Added dynamic commands

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+var dropzone;
+var grid;
+var number;
+var title;
+
 function setup() {
-		noCanvas();
-		
-		dropzone = select('#dropzone');
-		dropzone.drop(loadFiles);
+	noCanvas();
+	dropzone = select('#dropzone');
+	dropzone.drop(loadFiles);
+	title = select('#titolo')
+	grid = select('#cont');
+	number = select('#cols');
+	number.changed(updateCols);
+	number.elt.value = 5;
+	updateCols();
 }
 
 function loadFiles(event) {
-		for(let file of event.target.files){
-				var cont = createDiv();
-				cont.parent(document.getElementById('cont'));
-				var image = createImg(URL.createObjectURL(file), '');
-				image.parent(cont);
-				var p = createP(file.name);
-				p.parent(cont);
-		}
+	for (let file of event.target.files) {
+		var cont = createDiv();
+		cont.parent(document.getElementById('cont'));
+		var image = createImg(URL.createObjectURL(file), '');
+		image.parent(cont);
+		var p = createP(file.name);
+		p.parent(cont);
+	}
+}
+
+function updateCols() {
+	grid.attribute('style', '--cols: ' + number.elt.value + ';');
+}
+
+function empty() {
+	grid.elt.innerHTML = '';
+	title.elt.value = '';
 }
