094e08660e955ccfcabf62182ce815347260819d
Nazzareno Pasqual <79537141+TerribleCoding@users.noreply.github.com>
2021-04-23 19:41:53 +0200

    Added checkbox and js logic
    Images overflow from the container, or the container resizes in the wrong direction

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ number.value = 5;
 
 var title = document.getElementById('titolo');
 
+var squareCheck = document.getElementById('square');
+
 updateCols();
 
 function loadFiles(event) {
 	var files = event.target.files;
 	for (let i = 0; i < files.length; i++) {
 		var cont = document.createElement("div");
+		if (squareCheck.checked == true) { cont.classList.add('squares'); }
 		grid.appendChild(cont);
 		var image = document.createElement("img");
 		image.src = URL.createObjectURL(files[i]);
@@ -31,4 +34,11 @@ function updateCols() {
 function empty() {
 	grid.innerHTML = '';
 	title.value = '';
+}
+
+function squarify() {
+	var target = document.querySelectorAll('#cont div');
+	for (let i = 0; i < target.length; i++) {
+		target[i].classList.toggle('squares');
+	}
 }
