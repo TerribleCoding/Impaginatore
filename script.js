c11d817c0ff06133c967ab1493a8618bf5c40744
Nazzareno Pasqual <79537141+TerribleCoding@users.noreply.github.com>
2021-11-25 18:59:52 +0100

    Added img rotation and default square frames

--- script.js
+++ script.js
@@ -17,13 +17,28 @@ function loadFiles(event) {
 	var files = event.target.files;
 	for (let i = 0; i < files.length; i++) {
 		var cont = document.createElement("div");
-		if (squareCheck.checked == true) { cont.classList.add('squares'); }
 		grid.appendChild(cont);
+		var imgCont = document.createElement('div');
+		imgCont.classList.add('img-container');
+		if (squareCheck.checked == false) { imgCont.classList.add('squares'); }
 		var image = document.createElement("img");
 		image.src = URL.createObjectURL(files[i]);
+		image.setAttribute('style', '--rot: 0deg;');
+		var rotate = document.createElement('button');
+		rotate.classList.add('rotate');
+		rotate.onclick = function(event) {
+			var img = event.target.parentNode.parentNode.querySelector('img');
+			var rotation = img.getAttribute('style').match(/(--rot: )(\d+)(deg;)/).splice(1);
+			rotation[1] = parseInt(rotation[1]) + 90;
+			img.setAttribute('style', rotation.join(''));
+		};
+		var icon = document.createElement('img');
+		icon.src = 'rotate-right.png';
+		rotate.append(icon);
+		imgCont.append(image, rotate);
 		var p = document.createElement("p");
 		p.innerText = files[i].name;
-		cont.append(image, p);
+		cont.append(imgCont, p);
 	}
 }
 
@@ -37,7 +52,7 @@ function empty() {
 }
 
 function squarify() {
-	var target = document.querySelectorAll('#cont div');
+	var target = document.querySelectorAll('#cont .img-container');
 	for (let i = 0; i < target.length; i++) {
 		target[i].classList.toggle('squares');
 	}
